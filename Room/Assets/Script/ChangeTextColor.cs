fa7cee2b44d25ff3e547bb2843f5666413ddad76
yang.rice <rice.yang@outlook.com>
2016-06-30 23:25:55 +0800

    Add spec pictures: Headstock, Tailstock

---
+++ Room/Assets/Script/ChangeTextColor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+
+public class ChangeTextColor : MonoBehaviour {
+
+    Text text;
+    Color origin;
+    public Color color;
+
+    public void Change()
+    {
+        text.color = color;
+    }
+
+    public void Revert()
+    {
+        text.color = origin;
+    }
+
+    // Use this for initialization
+    void Start () {
+        text = gameObject.GetComponent<Text>();
+        origin = text.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
