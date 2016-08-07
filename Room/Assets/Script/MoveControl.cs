c97bfbcbd8d4de1c0a657e36a058f985ddaf934f
yang.rice <rice.yang@outlook.com>
2016-08-07 19:03:23 +0800

    Apply all motions by request
    Translate: X, Y, Z axis
    Rotation: C axis

--- Room/Assets/Script/MoveControl.cs
+++ Room/Assets/Script/MoveControl.cs
@@ -8,36 +8,43 @@ public class MoveControl : MonoBehaviour
 
     public Vector3 lowerBound = Vector3.zero;
     public Vector3 upperBound = Vector3.zero;
+    bool freezeX;
+    bool freezeY;
+    bool freezeZ;
 
     void Start()
     {
-        lowerBound = (lowerBound == Vector3.zero ? gameObject.transform.position : lowerBound);
-        upperBound = (upperBound == Vector3.zero ? gameObject.transform.position : upperBound);
+        freezeX = (lowerBound.x == upperBound.x);
+        freezeY = (lowerBound.y == upperBound.y);
+        freezeZ = (lowerBound.z == upperBound.z);
+        lowerBound = (lowerBound == Vector3.zero ? gameObject.transform.localPosition : lowerBound);
+        upperBound = (upperBound == Vector3.zero ? gameObject.transform.localPosition : upperBound);
     }
 
     void Update()
     {
-        if ((RigidbodyConstraints.FreezePositionX & gameObject.GetComponent<Rigidbody>().constraints) == 0)
+        const float tolerance = 0.01f;
+        if (!freezeX)
         {
             float upperX = upperBound.x - gameObject.transform.localPosition.x;
             float lowerX = lowerBound.x - gameObject.transform.localPosition.x;
-            if (upperX < 0.0f || lowerX > 0.0f) gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            if (upperX < -tolerance || lowerX > tolerance) gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
             if (upperX < 0.0f) gameObject.transform.Translate(upperX, 0, 0);
             if (lowerX > 0.0f) gameObject.transform.Translate(lowerX, 0, 0);
         }
-        if ((RigidbodyConstraints.FreezePositionY & gameObject.GetComponent<Rigidbody>().constraints) == 0)
+        if (!freezeY)
         {
             float upperY = upperBound.y - gameObject.transform.localPosition.y;
             float lowerY = lowerBound.y - gameObject.transform.localPosition.y;
-            if (upperY < 0.0f || lowerY > 0.0f) gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            if (upperY < -tolerance || lowerY > tolerance) gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
             if (upperY < 0.0f) gameObject.transform.Translate(0, upperY, 0);
             if (lowerY > 0.0f) gameObject.transform.Translate(0, lowerY, 0);
         }
-        if ((RigidbodyConstraints.FreezePositionZ & gameObject.GetComponent<Rigidbody>().constraints) == 0)
+        if (!freezeZ)
         {
             float upperZ = upperBound.z - gameObject.transform.localPosition.z;
             float lowerZ = lowerBound.z - gameObject.transform.localPosition.z;
-            if (upperZ < 0.0f || lowerZ > 0.0f) gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            if (upperZ < -tolerance || lowerZ > tolerance) gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
             if (upperZ < 0.0f) gameObject.transform.Translate(0, 0, upperZ);
             if (lowerZ > 0.0f) gameObject.transform.Translate(0, 0, lowerZ);
         }
@@ -58,4 +65,18 @@ public class MoveControl : MonoBehaviour
         gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, z);
     }
 
+    public void RotateX(float x)
+    {
+        gameObject.GetComponent<Rigidbody>().angularVelocity = new Vector3(x, 0, 0);
+    }
+
+    public void RotateY(float y)
+    {
+        gameObject.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, y, 0);
+    }
+
+    public void RotateZ(float z)
+    {
+        gameObject.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, z);
+    }
 }
